e426e7d766bb0deb57ffef1470c6edc79c8d6e3a
Simon Sessingo <simon.sessingoe@gmail.com>
2018-01-16 11:51:54 +0100

    Removed php 7 stuff from exception.

--- src/Pecee/Pixie/Exception.php
+++ src/Pecee/Pixie/Exception.php
@@ -14,7 +14,7 @@ class Exception extends \Exception
 
     protected $query;
 
-    public function __construct(string $message = '', int $code = 0, QueryObject $query = null)
+    public function __construct($message = '', $code = 0, $query = null)
     {
         parent::__construct($message, $code);
         $this->query = $query;
