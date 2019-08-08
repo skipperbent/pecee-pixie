55bac01c05b7b9ea925c3dfe7245dcaa688fec1f
André Vicente <adv@live.com.pt>
2019-08-08 11:56:42 +0100

    Create Sqlserver.php

---
+++ src/Pecee/Pixie/QueryBuilder/Adapters/Sqlserver.php
@@ -0,0 +1,14 @@
+<?php
+namespace Pecee\Pixie\QueryBuilder\Adapters;
+/**
+ * Class Sqlserver
+ *
+ * @package Pecee\Pixie\QueryBuilder\Adapters
+ */
+class Sqlserver extends BaseAdapter
+{
+    /**
+     * @var string
+     */
+    public const SANITIZER = '\';
+}
